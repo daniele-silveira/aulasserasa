6b8d0c798f4cb888c1034e4d26f7cb7e63d1e9ee
daniele-silveira <daniele20@gmail.com>
2022-04-07 17:17:14 -0300

    Interface

---
+++ danibank/AcessoInterno.java
@@ -0,0 +1,17 @@
+package danibank;
+
+public interface AcessoInterno {
+	
+	//os metodos não tem comportamento(corpo) so tem assinatura
+	
+	/**
+	 * @param String usuario => pode-se informar um usuario valido
+	 * @param String senha => pode-se informar uma senha com até 255 caracteres alfanumericos
+	 * @return retorna verdadeiro quando a senha estiver correta
+	 */
+	public boolean autenticarSistema(String usuario, String senha);
+	public boolean mudarSenha(String senhaAntiga, String senhaNova);
+	public boolean mudarUsuario(String usuarioAntigo, String usuarioNovo);
+
+
+}
