e9729c1780e1874c536998496df921f91547ffb2
daniele-silveira <daniele20@gmail.com>
2022-04-07 17:24:30 -0300

    While

---
+++ exercicios/EstruturaWhile.java
@@ -0,0 +1,18 @@
+//package serasa.transformese;
+
+//public class EstruturaWhile {
+//
+//	public static void main(String[] args) {
+//		
+//		//while
+//		int contador = 1;
+//
+//        while(contador < 10){
+//            System.out.println("Numero " + contador);
+//            //contador = contador + 2;
+//            contador = 2; // acumulador
+//        
+//	}
+//
+//}
+
